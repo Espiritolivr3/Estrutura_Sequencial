4bbbbc9069ee1a34cbff275940f1ae10e3f488ad
Espiritolivr3 <108704223+Espiritolivr3@users.noreply.github.com>
2022-07-25 14:18:00 -0300

    Add files via upload

---
+++ 07.py
@@ -0,0 +1,12 @@
+"""
+Faça um Programa que calcule a
+área de um quadrado, em seguida mostre
+o dobro desta área para o usuário
+"""
+
+lado_a = int(input('Digite o valor de um lado: '))
+lado_b = int(input('Digite o valor do outro lado: '))
+
+metro_quadrado = lado_a * lado_b
+
+print(f'O quadrado tem {metro_quadrado}m2')

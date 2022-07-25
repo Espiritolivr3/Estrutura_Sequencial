32f95663c1ef3de0181a89cc359432aec3dd43e1
Espiritolivr3 <108704223+Espiritolivr3@users.noreply.github.com>
2022-07-25 13:28:54 -0300

    Add files via upload

---
+++ 05.py
@@ -0,0 +1,23 @@
+""""
+Faça um Programa que converta metros para centímetros.
+"""
+while True:
+
+    metro = input('Digite o numero de metros: ')
+
+    if not metro.isdigit():
+        print('Valor invalido imbecil, por favor digite um numero')
+    else:
+        metro = int(metro)
+        resultado = metro * 100
+        if metro <= 1:
+            print(f'{metro} metro é igual a {resultado}cm')
+        else:
+            print(f'{metro} metros é igual a {resultado}cm')
+
+        break
+
+# elif metro <= 1:
+#    print(f'{metro} metro é igual a {resultado}cm')
+# else:
+#   print(f'{metro} metros é igual a {resultado}cm')
